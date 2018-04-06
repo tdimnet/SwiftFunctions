ed6ba0553962afd05f458171d4d0d15011394dc0
tdimnet <thomas.dimnet@gmail.com>
2018-04-06 18:23:28 +0200

    Made the swift fonction reusable.

--- SwiftFunctions.playground/Contents.swift
+++ SwiftFunctions.playground/Contents.swift
@@ -1,9 +1,4 @@
-/*
- ---------------
- Swift Functions
- ---------------
- */
-
+// Swift Functions
 
 // Area calculation for room #1
 //let length: Int = 10
@@ -18,13 +13,11 @@ let secondWidth: Int = 8
 
 let secondArea: Int = secondLength * secondWidth
 
-func area() {
-    let length: Int = 10
-    let width: Int = 12
+func area(length: Int, width: Int) {
     let areaOfRoom: Int = length * width
     print(areaOfRoom)
 }
 
-area()
-area()
-area()
+area(length: 10, width: 12)
+area(length: 15, width: 22)
+area(length: 23, width: 5)
