38f11796392426a2412d6ab68e3fa1c1884f5144
tdimnet <thomas.dimnet@gmail.com>
2018-04-06 11:18:46 +0200

    Add the first function in Swift.

--- SwiftFunctions.playground/Contents.swift
+++ SwiftFunctions.playground/Contents.swift
@@ -1,5 +1,30 @@
-//: Playground - noun: a place where people can play
+/*
+ ---------------
+ Swift Functions
+ ---------------
+ */
 
-import UIKit
 
-var str = "Hello, playground"
+// Area calculation for room #1
+//let length: Int = 10
+//let width: Int = 10
+//
+//let areaOfRoom: Int = length * width
+
+// Area calculation for room #2
+
+let secondLength: Int = 14
+let secondWidth: Int = 8
+
+let secondArea: Int = secondLength * secondWidth
+
+func area() {
+    let length: Int = 10
+    let width: Int = 12
+    let areaOfRoom: Int = length * width
+    print(areaOfRoom)
+}
+
+area()
+area()
+area()
